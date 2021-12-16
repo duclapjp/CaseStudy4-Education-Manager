55360de03df7e8d5e37ac3fd5a5aa1238d412aa5
unknown <khanhsan03@gmail.com>
2021-12-16 15:55:22 +0700

    fix role

--- src/main/java/com/codegym/educationmanager/repository/IRoleRepository.java
+++ src/main/java/com/codegym/educationmanager/repository/IRoleRepository.java
@@ -6,9 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRoleRepository extends JpaRepository<Role, Long> {
-<<<<<<< HEAD
 }
-=======
-    Role findByName(String name);
-}
->>>>>>> 210e4a86f174a8cb99127d3bc31d5e77cadab46c
+
