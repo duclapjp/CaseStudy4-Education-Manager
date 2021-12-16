2f1b6f9a805046a4fd8652afd711e1032c561b8d
unknown <khanhsan03@gmail.com>
2021-12-16 15:16:04 +0700

    crud-role

---
+++ src/main/java/com/codegym/educationmanager/repository/IRoleRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.educationmanager.repository;
+
+import com.codegym.educationmanager.model.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/codegym/educationmanager/service/role/RoleService.java
@@ -0,0 +1,32 @@
+package com.codegym.educationmanager.service.role;
+
+import com.codegym.educationmanager.model.role.Role;
+import com.codegym.educationmanager.repository.IRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class RoleService implements IRoleService{
+    @Autowired
+    private IRoleRepository roleRepository;
+    @Override
+    public Iterable<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> findById(Long id) {
+        return roleRepository.findById(id);
+    }
+
+    @Override
+    public Role save(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+         roleRepository.deleteById(id);
+    }
+}
